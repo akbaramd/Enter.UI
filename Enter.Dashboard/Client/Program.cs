afed76a59c51d310d1794612bfdc18da70949e58
akbarsafari00@gmail.com <akbarsafari00@gmail.com>
2023-09-21 13:45:19 +0330

    update

--- Enter.Dashboard/Client/Program.cs
+++ Enter.Dashboard/Client/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OptimumDashboard;
-using Pro.Dashboard;
+using Enter.Dashboard;
 
 namespace OptimumDashboard
 {
