3ae8ab65fa91a5eb295a4972cd9259983e6a438e
akbarsafari00@gmail.com <akbarsafari00@gmail.com>
2023-09-26 14:10:47 +0330

    .

--- Enter.UI/Components/EntIcon.razor.cs
+++ Enter.UI/Components/EntIcon.razor.cs
@@ -3,55 +3,48 @@ using global::System.Collections.Generic;
 using global::System.Linq;
 using global::System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Web;
-using Enter.UI.Components.Controllers;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
-using Enter.UI.Bases;
 using Enter.UI.Core;
 using System.ComponentModel.DataAnnotations;
+using Enter.UI.Core.Bases;
 
 namespace Enter.UI.Components
 {
     public partial class EntIcon : EntBaseComponent
     {
-
         public EntIcon()
         {
-            
         }
-        public EntIcon(EntIconType type,string data)
+
+        public EntIcon(EntIconType type, string data)
         {
             Data = data;
             Type = type;
         }
 
-        [Required]
-        [Parameter]
-        public string Data { get; set; }
+        [Required] [Parameter] public string Data { get; set; }
 
+        [Parameter] public EntIconType Type { get; set; } = EntIconType.IconTag;
 
-        [Parameter]
-        public EntIconType Type { get; set; } = EntIconType.IconTag;
+        protected string RootCss => CssClassBuilder
+            .AddClass("ent-icon")
+            .AddClass($"ent-icon-tag", Type == EntIconType.IconTag)
+            .AddClass($"ent-icon-image-tag", Type == EntIconType.ImageTag)
+            .AddClass($"ent-icon-svg-content", Type == EntIconType.SvgContent)
+            .Build();
 
-       
-        protected string cssClass => new CssClassBuilder("ent-icon")
-           .AddClass($"ent-icon-tag", Type == EntIconType.IconTag)
-           .AddClass($"ent-icon-image-tag", Type == EntIconType.ImageTag)
-           .AddClass($"ent-icon-svg-content", Type == EntIconType.SvgContent)
-           .AddClass(Class)
-           .Build();
-
-
-        public static EntIcon Create(EntIconType type , string data)
+        public static EntIcon Create(EntIconType type, string data)
         {
             return new EntIcon(type, data);
         }
-
     }
 
 
     public enum EntIconType
     {
-       ImageTag , IconTag, SvgContent
+        ImageTag,
+        IconTag,
+        SvgContent
     }
 }
--- Enter.UI/Components/EntTab.razor.cs
+++ Enter.UI/Components/EntTab.razor.cs
@@ -3,129 +3,127 @@ using global::System.Collections.Generic;
 using global::System.Linq;
 using global::System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Web;
-using Enter.UI.Components.Controllers;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
-using Enter.UI.Bases;
 using Enter.UI.Core;
+using Enter.UI.Core.Bases;
 
 namespace Enter.UI.Components
 {
     public partial class EntTab : EntTabBase
     {
+        protected string RootCss => CssClassBuilder
+            .AddClass("ent-tab")
+            .AddClass($"ent-tab-horizontal", Direction == EntTabDirection.Horizontal)
+            .AddClass($"ent-tab-vertical", Direction == EntTabDirection.Vertical)
+            .AddClass($"ent-tab-expandable", Expandable)
+            .Build();
 
+        protected string ItemCss => CssClassBuilder
+            .AddClass("ent-tab-item")
+            .AddClass($"ent-tab-item-horizontal", ItemDirection == EntTabItemDirection.Horizontal)
+            .AddClass($"ent-tab-item-vertical", ItemDirection == EntTabItemDirection.Vertical)
+            .AddClass(ItemClass)
+            .Build();
 
-        protected string tabCssClass => new CssClassBuilder("ent-tab")
-          .AddClass($"ent-tab-horizontal", Direction == EntTabDirection.Horizontal)
-          .AddClass($"ent-tab-vertical", Direction == EntTabDirection.Vertical)
-          .AddClass($"ent-tab-expandable", Direction == EntTabDirection.Vertical)
-          .AddClass(Class)
-          .Build();
+        protected string PanelCss => CssClassBuilder
+            .AddClass("ent-tab-panel-container")
+            .AddClass(PanelClass)
+            .AddClass($"ent-tab-panel-container-collapse", Expandable && ActiveTabId == null)
+            .Build();
 
-        protected string tabItemCssClass => new CssClassBuilder("ent-tab-item")
-         .AddClass($"ent-tab-item-horizontal", ItemDirection == EntTabItemDirection.Horizontal)
-         .AddClass($"ent-tab-item-vertical", ItemDirection == EntTabItemDirection.Vertical)
-          .AddClass(ItemClass)
-         .Build();
-
-        protected string tabPanelCssClass => new CssClassBuilder("ent-tab-panel-container")
-         .AddClass(PanelClass)
-         .AddClass($"ent-tab-panel-container-collapse", Expandable && ActiveTabId == null)
-         .Build();
-
-        [Parameter]
-        public RenderFragment ChildContent { get; set; }
-
-
+        [Parameter] public RenderFragment ChildContent { get; set; }
         private List<EntTabPanel> TabPanels = new List<EntTabPanel>();
-        private Guid? ActiveTabId = null;
-
+        private string? ActiveTabId = null;
 
         public async Task AddNewTab(EntTabPanel panel)
         {
             TabPanels.Add(panel);
 
             if (TabPanels.Count == 1)
-                ActiveTab(panel.TabId);
+                ActiveTab(panel.Id);
 
             OnTabAdded.InvokeAsync(panel).GetAwaiter().GetResult();
             StateHasChanged();
         }
 
-        public void ActiveTab(Guid? id)
+        public void ActiveTab(string? id)
         {
+            Console.WriteLine(id);
+            if (id == null)
+            {
+                return;
+            }
 
-            if (!id.HasValue || id.Value == Guid.Empty) { return; }
-
-            var panel = TabPanels.FirstOrDefault(x => x.TabId.Equals(id.Value));
+            var panel = TabPanels.FirstOrDefault(x => x.Id.Equals(id));
 
             if (panel == null) return;
 
-            if (Expandable && ActiveTabId.HasValue && ActiveTabId.Value == panel.TabId)
+            if (Expandable && ActiveTabId != null && ActiveTabId == panel.Id)
             {
                 ActiveTabId = null;
             }
             else
             {
-                ActiveTabId = panel.TabId;
+                ActiveTabId = panel.Id;
             }
 
             OnTabActived.InvokeAsync(ActiveTabId).GetAwaiter().GetResult();
             StateHasChanged();
         }
-        public void RemoveTab(Guid? id)
+
+        public void RemoveTab(string? id)
         {
-            if (!id.HasValue || id.Value == Guid.Empty) { return; }
-            var panel = TabPanels.FirstOrDefault(x => x.TabId.Equals(id.Value));
+            if (id == null)
+            {
+                return;
+            }
+
+            var panel = TabPanels.FirstOrDefault(x => x.Id.Equals(id));
             if (panel == null) return;
 
             TabPanels.Remove(panel);
             StateHasChanged();
         }
-        public bool IsActivePanel(Guid id)
+
+        public bool IsActivePanel(string? id)
         {
-            return ActiveTabId != null && ActiveTabId.Value.Equals(id);
+            if (id == null)
+            {
+                return false;
+            }
+            return ActiveTabId != null && ActiveTabId.Equals(id);
         }
 
-        private void OnTabClose(Guid id) =>
+        private void OnTabClose(string id) =>
             OnTabClosed.InvokeAsync(id).GetAwaiter().GetResult();
     }
 
     public enum EntTabDirection
     {
-        Vertical, Horizontal
+        Vertical,
+        Horizontal
     }
 
     public enum EntTabItemDirection
     {
-        Vertical, Horizontal
+        Vertical,
+        Horizontal
     }
 
     public class EntTabBase : EntBaseComponent
     {
-        [Parameter]
-        public bool KeepPanelAlive { get; set; } = false;
-        [Parameter]
-        public bool Closeable { get; set; } = false;
-        [Parameter]
-        public string ItemClass { get; set; }
-        [Parameter]
-        public string PanelClass { get; set; }
-        [Parameter]
-        public string ActiveClass { get; set; }
-        [Parameter]
-        public bool Expandable { get; set; } = false;
-        [Parameter]
-        public EntTabDirection Direction { get; set; } = EntTabDirection.Vertical;
-        [Parameter]
-        public EntTabItemDirection ItemDirection { get; set; } = EntTabItemDirection.Horizontal;
+        [Parameter] public bool KeepPanelAlive { get; set; } = false;
+        [Parameter] public bool Closeable { get; set; } = false;
+        [Parameter] public string ItemClass { get; set; }
+        [Parameter] public string PanelClass { get; set; }
+        [Parameter] public string ActiveClass { get; set; }
+        [Parameter] public bool Expandable { get; set; } = false;
+        [Parameter] public EntTabDirection Direction { get; set; } = EntTabDirection.Vertical;
+        [Parameter] public EntTabItemDirection ItemDirection { get; set; } = EntTabItemDirection.Horizontal;
 
-        [Parameter]
-        public EventCallback<Guid?> OnTabActived { get; set; }
-        [Parameter]
-        public EventCallback<EntTabPanel> OnTabAdded { get; set; }
-        [Parameter]
-        public EventCallback<Guid> OnTabClosed { get; set; }
-
+        [Parameter] public EventCallback<string?> OnTabActived { get; set; }
+        [Parameter] public EventCallback<EntTabPanel> OnTabAdded { get; set; }
+        [Parameter] public EventCallback<string> OnTabClosed { get; set; }
     }
 }
