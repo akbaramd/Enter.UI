e599b15a4464924a3252a82adc360b2749390c15
akbarsafari00@gmail.com <akbarsafari00@gmail.com>
2023-10-07 13:44:35 +0330

    .

---
+++ Enter.Dashboard/Model/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace Enter.Dashboard.Model;
+
+public class Users
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
---
+++ Enter.UI/Components/Table/EntTable.razor.cs
@@ -0,0 +1,70 @@
+﻿using Enter.UI.Core;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enter.UI.Components.Contracts;
+using Enter.UI.Core.Bases;
+using Enter.UI.Services.Contracts;
+using Microsoft.JSInterop;
+
+namespace Enter.UI.Components
+{
+    public partial class EntTable<T> : EntBaseComponent
+    {
+        protected string RootCss => CssClassBuilder.AddClass("ent-table")
+            .Build();
+
+        private int? _currentCount => DataSources?.Count;
+        
+        private int _currentPage = 1;
+        private int _totalPage => (int) Math.Ceiling(decimal.Parse(Total.ToString()) / decimal.Parse(Take.ToString()));
+
+
+        private bool showLastPaginate = true;
+        private bool showNextPaginate = true;
+        
+        [Parameter] public RenderFragment? HeaderTemplate { get; set; }
+        [Parameter] public RenderFragment<T>? RowTemplate { get; set; }
+        [Parameter] public List<T>? DataSources { get; set; }
+        
+        [Parameter] public int Total { get; set; }
+        [Parameter] public int Take { get; set; }
+        
+        
+        [Parameter] public EventCallback<int> OnPaginationChange { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            DoLogic();
+            await base.OnInitializedAsync();
+        }
+
+        public async Task NextPage()
+        {
+            _currentPage++;
+            DoLogic();
+            await OnPaginationChange.InvokeAsync(_currentPage);
+        }
+        
+        public async Task LastPage()
+        {
+            _currentPage--;
+            DoLogic();
+            await OnPaginationChange.InvokeAsync(_currentPage);
+        }
+
+        
+        public void DoLogic()
+        {
+            Console.WriteLine(_currentPage);
+            showLastPaginate = _currentPage != 1;
+            showNextPaginate = _currentPage != _totalPage;
+            StateHasChanged();
+        }
+        
+    }   
+}
