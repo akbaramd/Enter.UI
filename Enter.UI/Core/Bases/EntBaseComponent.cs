521c15eab57f41d8a0457c5262e0248ec343573c
Akbar Ahmadi Saray <akbarsafari00@gmail.ciom>
2023-10-14 07:17:44 +0330

    .

--- Enter.UI/Core/Bases/EntBaseComponent.cs
+++ Enter.UI/Core/Bases/EntBaseComponent.cs
@@ -9,14 +9,14 @@ namespace Enter.UI.Core.Bases
         //todo : Remove Style and Create Style Builder
         
         [Parameter(CaptureUnmatchedValues =true)]
-        public Dictionary<string, object?> Attributes { get; set; } = new Dictionary<string, object?>();
+        public Dictionary<string, object?> AdditionalAttributes { get; set; } = new Dictionary<string, object?>();
 
         protected CssClassBuilder CssClassBuilder =>
-            new CssClassBuilder().AddClass(Attributes.TryGetValue("class", out var @class) ? @class.ToString() : string.Empty);
+            new CssClassBuilder().AddClass(AdditionalAttributes.TryGetValue("class", out var @class) ? @class.ToString() : string.Empty);
         
         protected StyleBuilder StyleBuilder =>
-            new StyleBuilder().AddStyle(Attributes.TryGetValue("style", out var @style) ? @style.ToString() : string.Empty);
-        public string Id => (Attributes?.ContainsKey("id") == true ? Attributes["id"]?.ToString() ?? $"ent-{Guid.NewGuid()}" : $"ent-{Guid.NewGuid()}");
+            new StyleBuilder().AddStyle(AdditionalAttributes.TryGetValue("style", out var @style) ? @style.ToString() : string.Empty);
+        public string Id => (AdditionalAttributes?.ContainsKey("id") == true ? AdditionalAttributes["id"]?.ToString() ?? $"ent-{Guid.NewGuid()}" : $"ent-{Guid.NewGuid()}");
 
     }
 }
