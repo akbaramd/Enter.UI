c1f6d4660d25647402bb3c4bf5edd650f94b9820
Akbar Ahmadi Saray <akbarsafari00@gmail.ciom>
2023-10-22 00:55:43 +0330

    add text component

---
+++ Enter.UI/Components/Text/EntTextType.cs
@@ -0,0 +1,13 @@
+namespace Enter.UI.Components;
+
+
+public enum EntTextType
+{
+    Text,Bold,Paragraph,Small,Heading1,Heading2,Heading3,Heading4,Heading5,Heading6
+}
+
+
+public enum EntTextSize
+{
+    Medium,Title,Subtitle,Description,Caption,SubHeading,Heading
+}
--- Enter.UI/Extensions.cs
+++ Enter.UI/Extensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,7 +31,11 @@ namespace Enter.UI
             services.AddSingleton<IEntLayoutJsService, EntLayoutJsService>();
         }
         
-  
+        public static string GetPropertyName<TProp>(this Expression<Func<TProp>> expression)
+        {
+            var memberExpression = (MemberExpression)expression.Body;
+            return memberExpression.Member.Name;
+        }
         
     }
 }
