34b512154ca46adf3f72386f4201ef7651ef448a
Akbar Ahmadi Saray <akbarsafari00@gmail.ciom>
2023-11-02 07:56:57 +0330

    work on tree view (have bug)

--- example/Enter.Dashboard/Model/Roles.cs
+++ example/Enter.Dashboard/Model/Roles.cs
@@ -8,6 +8,7 @@ public class Roles  : IEntTreeViewItem<Roles,string>
     public Guid Id { get; set; }
     public string Text { get; set; }
     public string? Icon { get; set; }
+    public bool Selected { get; set; } = false;
     public string Value { get; set; }
     public IEnumerable<Roles> Childrens { get; set; }
     public Guid ParentId { get; set; }
--- src/Enter.UI.Abstractions/Components/TreeView/IEntTreeViewItem.cs
+++ src/Enter.UI.Abstractions/Components/TreeView/IEntTreeViewItem.cs
@@ -5,6 +5,7 @@ public interface IEntTreeViewItem<TType,TValue> where TType:IEntTreeViewItem<TTy
     public TValue Value  { get; set; }
     public string Text  { get; set; }
     public string? Icon  { get; set; }
-    
+
+    public bool Selected { get; set; }
     public IEnumerable<TType>? Childrens { get; set; }
 }
