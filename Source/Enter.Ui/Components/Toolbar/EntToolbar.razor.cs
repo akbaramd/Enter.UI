f4f086f80d887e2eedc060d2b35e834ad3393e63
akbarsafari00@gmail.com <akbarsafari00@gmail.com>
2023-12-03 13:49:38 +0330

    fix nav menu and toolbar

--- Source/Enter.Ui/Components/Toolbar/EntToolbar.razor.cs
+++ Source/Enter.Ui/Components/Toolbar/EntToolbar.razor.cs
@@ -8,6 +8,19 @@ namespace Enter.Ui.Components;
 
 public partial class EntToolbar : EntBaseComponent
 {   public override string ComponentName => this.GetType().Name;
+
+    [Parameter]
+    public RenderFragment? StartContent { get; set; }
+    [Parameter]
+    public RenderFragment? EndContent { get; set; }
+    [Parameter]
+    public RenderFragment? ExpandContent { get; set; }
+    
+    [Parameter]
+    public bool Expanded { get; set; }
+    [Parameter]
+    public EventCallback<bool> ExpandedChanged { get; set; }
+    
     protected override void BuildClasses(ClassBuilder builder)
     {
         builder.AddClass("ent-toolbar");
